8e988f5ca3bb75f8a18c4dcbea2d201777aa52eb
veg-str <veg_str@outlook.com>
2020-01-31 16:53:19 +0300

    Added rb_vip IP address to PSM vars script

--- psm_vars.py
+++ psm_vars.py
@@ -6,8 +6,20 @@ vars_dir = 'c:\\temp\\host_vars\\'
 # Open Excel file in read-only mode
 wb = openpyxl.load_workbook(ip_plan, True)
 
-mr = ['spb', 'mos', 'ros', 'nin', 'ekt', 'nsk']
+mr = ['spb', 'nin', 'ekt', 'nsk', 'ros', 'mos']
 quorum = '2'
+
+
+def rb_vip():
+    rb_vip = {}
+    for region in mr:
+        rb_reg = {}
+        ws = wb[region.upper()]
+        for row in ws.iter_rows():
+            if re.search("^rb\d\d.*\(VRRP VIP\)", str(row[1].value)) and row[3].value == 'Radius':
+                rb_reg[re.search("^rb\d\d\." + region, str(row[1].value)).group(0)] = row[5].value
+        rb_vip[region] = rb_reg
+    return rb_vip
 
 
 def prefix(mr, vlan):
@@ -21,7 +33,6 @@ def prefix(mr, vlan):
     return prefix
 
 
-
 def psm_list(mr):
     ws = wb[mr.upper()]
     psms = []
@@ -29,6 +40,7 @@ def psm_list(mr):
         if row[3].value == 'vm-Mgmt' and re.search("^psm", row[1].value):
             psms.append(row[1].value)
     return psms
+
 
 def psm_vars(mr, psm):
     ws = wb[mr.upper()]
@@ -46,13 +58,14 @@ def psm_vars(mr, psm):
     return psm_vars
 
 
+rb = rb_vip()
 for item in mr:
     psms = psm_list(item)
 #    print(psms)
     for psm in psms:
         vars = psm_vars(item, psm)
 #        print(vars)
-        var_file = vars_dir + psm + '.yml'
+        var_file = vars_dir + psm[:10] + '.yml'
         with open(var_file, 'w', newline='\n') as f:
             f.write('# Variables for ' + psm + '\n#\n')
             f.write('# High availability related vars\n#\n')
@@ -71,7 +84,12 @@ for item in mr:
             f.write('aaa_ip: ' + vars[psm[:-13]]['Radius'] + '\n')
             f.write('res_ip: ' + vars[psm[:-13]]['Resource'] + '\n')
             f.write('provisioning_ip: ' + vars[psm[:-13]]['Provisioning'] + '\n')
-            f.write('cluster_sync_ip: ' + vars[psm[:-13]]['ClusterSync'])
-        print(vars)
+            f.write('cluster_sync_ip: ' + vars[psm[:-13]]['ClusterSync'] + '\n')
+            f.write('#\n' + '# Other vars\n#\n')
+            if int(psm[3:5]) % 2 == 0:
+                f.write('rb_vip: ' + rb[item]['rb02.' + item] + '\n')
+            else:
+                f.write('rb_vip: ' + rb[item]['rb01.' + item] + '\n')
+            print(vars)
 
 print(yaml.dump(vars[list(vars.keys())[0]]))
