13e64a8fa6f9421200a28bdba8303ec090a4dda8
veg_s <ve.gusarin@jet.su>
2020-08-14 16:59:16 +0300

    Added dom_num var to inventory files.

--- create_mgmtsoft_inventory.py
+++ create_mgmtsoft_inventory.py
@@ -1,141 +1,114 @@
 # -*- coding: utf-8 -*-
-import openpyxl, re, datetime
+import openpyxl
+import re
+from datetime import datetime
 import lxml.etree as xml
+from shared import ip_plan
 
-#mr = ['ekt']
-mr = ['ekt', 'mos', 'nin', 'nsk', 'ros', 'spb']
+mr = ['MOS','EKT', 'NIN', 'NSK']
+# mr = ['MOS','EKT', 'NIN', 'NSK', 'SPB', 'ROS']
 base_srv_types = ['pre', 'psm', 'pic', 'apic']
 ext_srv_types = ['epsm', 'rb', 'log', 'rs']
-ip_plan = 'project_files\\Tele2_IP_plan_v2.02.xlsx'
-wb = openpyxl.load_workbook(ip_plan, True)
+ip_plan_file = 'project_files\\Tele2_IP_plan_v2.04-draft.xlsx'
+wb = openpyxl.load_workbook(ip_plan_file, True)
+
+f_dir = 'c:\\temp\\mgmt_soft\\'
 
 
-def get_srv_list():
-#    ip_plan = 'project_files\\Tele2_IP_plan_v044.xlsx'
-#    wb = openpyxl.load_workbook(ip_plan, True)
-    host_list = {}
-    vm_list = {}
-    for region in mr:
-        ws = wb[region.upper()]
-        h = []
-        vm = []
-        for row in ws.iter_rows():
-            if row[3].value == 'Host_Mgmt':
-                h.append({'hostname': row[0].value, 'ip': row[5].value, 'site': row[6].value})
-            elif row[3].value == 'vm_Mgmt':
-                vm.append({'vmname': row[1].value, 'ip': row[5].value, 'site': row[6].value})
-        host_list[region] = h
-        vm_list[region] = vm
-    return host_list, vm_list
-
-
-def get_sw_list():
-#    ip_plan = 'project_files\\Tele2_IP_plan_v044.xlsx'
-#    wb = openpyxl.load_workbook(ip_plan, True)
-    sw_list = {}
-    for region in mr:
-        net = wb.defined_names[region + '_nets'].attr_text
-        ws = wb[str(net[1:net.find('!') - 1])]
-        rng = net[net.find('!') + 1:]
-        sw = []
-        for row in ws[rng]:
-            if row[0].value == 'OOB_Mgmt':
-                sw.append({'swname': region.upper() + '-TMS-1-1', 'ip': row[6].value, 'site': 'Site1'})
-                sw.append({'swname': region.upper() + '-TMS-2-1', 'ip': row[9].value, 'site': 'Site2'})
-        sw_list[region] = sw
-    return sw_list
-
-
-def create_superputty_inv():
-    """
-    Creating XML file for SuperPutty
-    """
-    dir = 'c:\\temp\\mgmt_soft\\'
-#    filename = dir + 'SuperPutty_test.xml'
-    filename = dir + 'SuperPutty_' + str(
-                datetime.datetime.today().isoformat(sep='_', timespec='minutes')).replace(':', '_') + '.xml'
+def create_superputty_inv(root):
+    global host_list
+    global vm_list
+    global sw_list
+    global dom_num
     attr = {}
-    root = xml.Element("ArrayOfSessionData")
-    for region in mr:
-        for sw in switches[region]:
-            attr['SessionId'] = 'Tele2-TMS/' + region.upper() + '/' + sw['site'] + '/' + sw['swname']
-            attr['SessionName'] = sw['swname']
-            attr['ImageKey'] = 'drive_network'
-            attr['Host'] = sw['ip']
-            attr['Port'] = '22'
-            attr['Proto'] = 'SSH'
-            attr['PuttySession'] = "Default Settings"
-            attr['Username'] = 'jet'
-            attr['ExtraArgs'] = ''
-            attr['SPSLFileName'] = ''
-            xml.SubElement(root, "SessionData", attrib=attr)
-        for srv in hosts[region]:
-            attr['SessionId'] = 'Tele2-TMS/' + region.upper() + '/' + srv['site'] + '/Hosts/' + srv['hostname']
-            attr['SessionName'] = srv['hostname']
-            attr['ImageKey'] = 'computer'
-            attr['Host'] = srv['ip']
-            attr['Port'] = '22'
-            attr['Proto'] = 'SSH'
-            attr['PuttySession'] = "Default Settings"
+    for sw in sw_list:
+        attr['SessionId'] = f'Tele2-TMS/{region.upper()}/Domain{dom_num}/{sw["swname"]}'
+        attr['SessionName'] = sw['swname']
+        attr['ImageKey'] = 'drive_network'
+        attr['Host'] = sw['ip']
+        attr['Port'] = '22'
+        attr['Proto'] = 'SSH'
+        attr['PuttySession'] = "Default Settings"
+        attr['Username'] = 'jet'
+        attr['ExtraArgs'] = ''
+        attr['SPSLFileName'] = ''
+        xml.SubElement(root, "SessionData", attrib=attr)
+    for srv in host_list:
+        attr['SessionId'] = f'Tele2-TMS/{region.upper()}/Domain{dom_num}/{srv["site"]}/Hosts/{srv["hostname"]}'
+        attr['SessionName'] = srv['hostname']
+        attr['ImageKey'] = 'computer'
+        attr['Host'] = srv['ip']
+        attr['Port'] = '22'
+        attr['Proto'] = 'SSH'
+        attr['PuttySession'] = "Default Settings"
+        attr['Username'] = 'root'
+        attr['ExtraArgs'] = ''
+        attr['SPSLFileName'] = ''
+        xml.SubElement(root, "SessionData", attrib = attr)
+    for srv in vm_list:
+        type = re.search("^\D{1,4}", srv['vm']).group(0)
+        if type in base_srv_types:
+            type = type.upper()
+            attr['Port'] = '42002'
+            attr['Username'] = 'pladmin'
+        else:
+            type = 'Other'
             attr['Username'] = 'root'
-            attr['ExtraArgs'] = ''
-            attr['SPSLFileName'] = ''
-            xml.SubElement(root, "SessionData", attrib = attr)
-        for srv in vms[region]:
-            type = re.search("^\D{1,4}", srv['vmname']).group(0)
-            if type in base_srv_types:
-                type = type.upper()
-                attr['Port'] = '42002'
-                attr['Username'] = 'pladmin'
-            else:
-                type = 'Other'
-                attr['Username'] = 'root'
-                attr['Port'] = '22'
-            attr['SessionId'] = 'Tele2-TMS/' + region.upper() + '/' + srv['site'] + '/' + type + '/' + srv['vmname']
-            attr['SessionName'] = srv['vmname']
-            attr['ImageKey'] = 'computer'
-            attr['Host'] = srv['ip']
-            attr['Proto'] = 'SSH'
-            attr['PuttySession'] = "Default Settings"
-            attr['ExtraArgs'] = ''
-            attr['SPSLFileName'] = ''
-            xml.SubElement(root, "SessionData", attrib = attr)
-    tree = xml.ElementTree(root)
-    with open(filename, "wb") as file:
-        tree.write(file, pretty_print=True, encoding="utf-8", xml_declaration=True)
+            attr['Port'] = '22'
+        attr['SessionId'] = f'Tele2-TMS/{region.upper()}/Domain{dom_num}/{srv["site"]}/{type}/{srv["vm"]}'
+        attr['SessionName'] = srv['vm']
+        attr['ImageKey'] = 'computer'
+        attr['Host'] = srv['ip']
+        attr['Proto'] = 'SSH'
+        attr['PuttySession'] = "Default Settings"
+        attr['ExtraArgs'] = ''
+        attr['SPSLFileName'] = ''
+        xml.SubElement(root, "SessionData", attrib = attr)
+    return root
 
 
 def create_plclient_inv():
-    """
-    Creating XML file for SuperPutty
-    """
-    dir = 'c:\\temp\\mgmt_soft\\'
-    filename = dir + 'PLClient_' + str(
-        datetime.datetime.today().isoformat(sep='_', timespec='minutes')).replace(':', '_') + '.psx'
     attr = {}
-    root = xml.Element("Systems", version = "2")
-    lvl1 = xml.SubElement(root, 'folder', name = "Tele2-TMS")
-    for region in mr:
-        lvl2 = xml.SubElement(lvl1, 'folder', name = region.upper())
-        for site in 'Site1', 'Site2':
-            lvl3 = xml.SubElement(lvl2, 'folder', name = site)
-            for type in base_srv_types:
-                lvl4 = xml.SubElement(lvl3, 'folder', name = type.upper())
-                for srv in vms[region]:
-                    t = re.search("^\D{1,4}", srv['vmname']).group(0)
-                    if srv['site'] == site and t == type:
-                        attr['username'] = "jet"
-                        attr['address'] = srv['ip']
-                        attr['defaultview'] = "SystemOverview"
-                        attr['name'] = srv['vmname']
-                        attr['Password'] = "00000008789c2b342c0443000bec0289"
-                        xml.SubElement(lvl4, 'system', attrib = attr)
-    tree = xml.ElementTree(root)
-    with open(filename, "wb") as file:
-        tree.write(file, pretty_print=True, encoding="utf-8", xml_declaration=False)
+    lvl3 = xml.Element('folder', name = f'Domain{dom_num}')
+    for site in sites:
+        lvl4 = xml.Element('folder', name=site)
+        for s_type in base_srv_types:
+            lvl5 = xml.Element('folder', name=s_type.upper())
+            for srv in vm_list:
+                t = re.search("^\D{1,4}", srv['vm']).group(0)
+                if srv['site'] == site and t == s_type:
+                    attr['username'] = "jet"
+                    attr['address'] = srv['ip']
+                    attr['defaultview'] = "SystemOverview"
+                    attr['name'] = srv['vm']
+                    attr['Password'] = "00000008789c2b342c0443000bec0289"
+                    xml.SubElement(lvl5, 'system', attrib=attr)
+            lvl4.append(lvl5)
+        lvl3.append(lvl4)
+    return lvl3
 
-switches = get_sw_list()
-hosts, vms = get_srv_list()
 
-create_superputty_inv()
-create_plclient_inv()
+sp_root = xml.Element("ArrayOfSessionData")
+pl_root = xml.Element("Systems", version = "2")
+pl_lvl1 = xml.SubElement(pl_root, 'folder', name = "Tele2-TMS")
+for region in mr:
+    sheets = ip_plan.get_sheets_list(region)
+    pl_lvl2 = xml.SubElement(pl_lvl1, 'folder', name = region.upper())
+    for sheet in sheets:
+        dom_num = sheets.index(sheet) + 1
+        host_list = list(filter(ip_plan.filter_host_only, ip_plan.rows_to_dict(sheet)))
+        vm_list = list(filter(ip_plan.filter_vm_only, ip_plan.rows_to_dict(sheet)))
+        sw_list = ip_plan.get_sw_list(sheet, dom_num)
+        sites = sorted(set(map(lambda i: i['site'], host_list)))
+        sp_root = create_superputty_inv(sp_root)
+        pl_lvl2.append(create_plclient_inv())
+
+tree = xml.ElementTree(sp_root)
+filename = f'{f_dir}SuperPutty_{str(datetime.today().isoformat(sep="_", timespec="minutes")).replace(":", "_")}.xml'
+with open(filename, "wb") as file:
+    tree.write(file, pretty_print=True, encoding="utf-8", xml_declaration=True)
+
+tree = xml.ElementTree(pl_root)
+filename = f'{f_dir}PLCient_{str(datetime.today().isoformat(sep="_", timespec="minutes")).replace(":", "_")}.xml'
+with open(filename, "wb") as file:
+    tree.write(file, pretty_print=True, encoding="utf-8", xml_declaration=True)
---
+++ shared/ip_plan.py
@@ -0,0 +1,50 @@
+import openpyxl
+import re
+
+ip_plan_file = 'project_files\\Tele2_IP_plan_v2.04-draft.xlsx'
+base_srv_types = ['pre', 'psm', 'pic', 'apic']
+ext_srv_types = ['epsm', 'rb', 'log', 'rs']
+
+wb = openpyxl.load_workbook(ip_plan_file, True)
+
+
+def get_sheets_list(region):
+    ws_list = list(filter(lambda i: re.search('^'+region, i), wb.sheetnames))
+    return ws_list
+
+
+def rows_to_dict(sheet):
+    rows = []
+    ws = wb[sheet]
+    for row in ws.iter_rows():
+        rows.append({
+            'hostname': row[0].value,
+            'vm': row[1].value,
+            'vlan': row[3].value,
+            'ip': row[5].value,
+            'site': row[6].value
+        })
+    return rows
+
+
+def filter_host_only(src):
+    res = src['vlan'] == 'Host_Mgmt'
+    return res
+
+
+def filter_vm_only(src):
+    res = src['vlan'] == 'vm_Mgmt'
+    return res
+
+
+def get_sw_list(sheet, dom_num):
+    wsid = wb.sheetnames.index('IP-plan')
+    net = wb.defined_names.get(sheet.lower(), wsid).attr_text
+    ws = wb[str(net[1:net.find('!') - 1])]
+    rng = net[net.find('!') + 1:]
+    sw_list = []
+    for row in ws[rng]:
+        if row[0].value == 'OOB_Mgmt':
+            sw_list.append({'swname': f'{sheet[:3].upper()}-TMS-1-{dom_num}', 'ip': row[6].value, 'site': 'Site1'})
+            sw_list.append({'swname': f'{sheet[:3].upper()}-TMS-2-{dom_num}', 'ip': row[9].value, 'site': 'Site2'})
+    return sw_list
